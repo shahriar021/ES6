4f87dd4b80bbbe65ecad0bc5186597f02bd5223d
shahriar021 <shahriarc19@gmail.com>
2023-09-03 00:20:42 +0600

    function

---
+++ arrowFunctions/script.js
@@ -0,0 +1,5 @@
+//old js 3 type er function thake 1.function expression 2.function statement 3.anonymous function
+//new js 1 type er function thake arrow function
+//1.basic arrow function
+//2.arrow function with parameter
+//3.arrow function with implicit return
---
+++ letNconst/script.js
@@ -0,0 +1,4 @@
+//let and const are block scoped let can be changed const cannot be changed 
+// let and const from es6
+//let and const are different
+//let and const similar in few things
