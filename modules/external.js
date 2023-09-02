8838ccc0bcea1e1040ad99acda6512afc00d7889
shahriar021 <shahriarc19@gmail.com>
2023-09-03 05:46:42 +0600

    modules

---
+++ modules/external.js
@@ -0,0 +1,10 @@
+
+export const name = "shahriar"
+
+export const position = "software engineer"
+
+
+
+export default function myFunc(){
+    console.log("shahriar'function")
+}
---
+++ modules/internal.js
@@ -0,0 +1,9 @@
+// import {name} from './external.js';// named import 
+// import * as all  from  './external.js' ;// for import all
+// import {name as n,position as p} from './external.js'
+
+import any,{name as n} from './external.js'//by default charao kisu ante caile
+
+console.log(any ,n)
+
+any();
